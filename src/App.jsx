b062adf5bc31b578bdeef960bb3013c3a80f80b0
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2024-08-25 22:45:35 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,39 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
   const [query, setQuery] = useState({ q: 'Odisha' });
   const [units, setUnits] = useState('metric');
-  const [weather, setWeather] = useState(null)
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     const fetchWeather = async () => {
-      const message = query.q ? query.q : 'current location'
+      const message = query.q ? query.q : 'current location';
 
       toast.info('Fetching weather for ' + message);
-      (await getFormattedWeatherData({ ...query, units }).then(
-        (data) => {
-          toast.success(`Successfully fetched weather for ${data.name}, ${data.country}.`)
-          setWeather(data);
-        }
-      ));
 
+      try {
+        const data = await getFormattedWeatherData({ ...query, units });
+        if (data) {
+          toast.success(`Successfully fetched weather for ${data.name}, ${data.country}.`);
+          setWeather(data);
+        } else {
+          toast.error('Unable to fetch weather data. Please try again later.');
+        }
+      } catch (error) {
+        toast.error(`Error fetching weather data: ${error.message}`);
+      }
     };
 
     fetchWeather();
   }, [query, units]);
 
   const formatBackground = () => {
-    if (!weather) return 'from-cyan-700 to-blue-700'
-    const threshold = units === 'metric' ? 20 : 60
-    if (weather.temp <= threshold) return 'from-cyan-700 to-blue-700'
+    if (!weather) return 'from-cyan-700 to-blue-700';
+    const threshold = units === 'metric' ? 20 : 60;
+    if (weather.temp <= threshold) return 'from-cyan-700 to-blue-700';
 
-    return 'from-yellow-700 to-orange-700'
-  }
+    return 'from-yellow-700 to-orange-700';
+  };
 
   return (
     <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700
@@ -56,7 +60,6 @@ function App() {
       )}
       <ToastContainer autoClose={5000} theme='colored' newestOnTop={true} />
     </div>
-
   );
 }
 
